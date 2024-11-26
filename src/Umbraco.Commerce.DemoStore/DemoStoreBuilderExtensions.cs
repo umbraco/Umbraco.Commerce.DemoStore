38a0d54024be01485fc9bbf0618324b2759986b0
mattbrailsford <me@mattbrailsford.com>
2024-11-26 08:49:23 +0000

    Support passing a load test flag to disable email sending

--- src/Umbraco.Commerce.DemoStore/DemoStoreBuilderExtensions.cs
+++ src/Umbraco.Commerce.DemoStore/DemoStoreBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Umbraco.Commerce.Core.Events.Notification;
+﻿using Microsoft.Extensions.Configuration;
+using Umbraco.Commerce.Core.Events.Notification;
 using Umbraco.Commerce.DemoStore.Events;
 using Umbraco.Commerce.DemoStore.Web.Extractors;
 using Umbraco.Commerce.Cms.Extractors;
@@ -6,6 +7,7 @@ using Umbraco.Commerce.Extensions;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Extensions;
 using Umbraco.Cms.Core.Notifications;
+using Umbraco.Commerce.Core.Events.Notification.Handlers.Order;
 
 namespace Umbraco.Commerce.DemoStore
 {
@@ -39,6 +41,14 @@ namespace Umbraco.Commerce.DemoStore
 
                 v.WithNotificationEvent<OrderShippingMethodChangingNotification>()
                     .RegisterHandler<OrderShippingMethodChangingHandler>();
+                
+                if (v.Config.GetValue<bool>("Umbraco:Commerce:DemoStore:LoadTest"))
+                {
+                    // If we are running a load test, we don't want to send emails
+                    v.WithNotificationEvent<OrderFinalizedNotification>()
+                        .RemoveHandler<SendFinalizedOrderEmail>()
+                        .RemoveHandler<SendGiftCardEmails>();
+                }
 
             });
 
